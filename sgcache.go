1576aed114a14dfdb8520c09667dd326cba4493e
Xushuchi <njuxushuchi@163.com>
2020-05-28 17:02:14 +0800

    完成了单机并发缓存

--- sgcache.go
+++ sgcache.go
@@ -1,5 +1,18 @@
 package sgcache
 
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// 缓存的命名空间
+type Group struct {
+	name      string
+	getter    Getter // 缓存未命中时获取源数据的回调
+	mainCache cache  // 并发缓存
+}
+
 type Getter interface {
 	Get(key string) ([]byte, error)
 }
@@ -9,3 +22,65 @@ func (f GetterFunc) Get(key string) ([]byte, error) {
 }
 
 type GetterFunc func(key string) ([]byte, error)
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter GetterFunc) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	// 从mainCache中查找缓存，如果存在则返回缓存值
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[SgCache] hit")
+		return v, nil
+	}
+
+	// 缓存不存在，则调用load方法
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	// 调用用户的回调方法获取源数据
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+// 将源数据添加到缓存
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
--- sgcache_test.go
+++ sgcache_test.go
@@ -1,9 +1,17 @@
 package sgcache
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
 
 func TestGetter(t *testing.T) {
 	// 使用GetterFunc将匿名函数转换为接口Getter
@@ -14,5 +22,34 @@ func TestGetter(t *testing.T) {
 	expect := []byte("key")
 	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
 		t.Errorf("callback failed")
+	}
+}
+
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db)) // 记录某个键调用回调函数的次数
+	sg := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key]++
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exists", key)
+		}))
+
+	for k, v := range db {
+		if view, err := sg.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		}
+		if _, err := sg.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+
+	if view, err := sg.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
 	}
 }
